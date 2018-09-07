24b0e48afc106371ec171de9d9c78f0e020f4b5d
DJ Yuhn <djyuhn@gmail.com>
2018-09-06 22:26:42 -0500

    ICP3 Complete Tasks
    Complete the getting of triplets and synonyms for the 10 abstracts.

--- KDM-ICP3/SparkOpenIE_WordNET_LDA/src/main/scala/openie/SparkOpenIE.scala
+++ KDM-ICP3/SparkOpenIE_WordNET_LDA/src/main/scala/openie/SparkOpenIE.scala
@@ -1,5 +1,7 @@
 package openie
 
+import java.io.{File, PrintWriter}
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -9,28 +11,33 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkOpenIE {
 
   def main(args: Array[String]) {
+
+    // For Windows Users
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+
     // Configuration
     val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
 
     val sc = new SparkContext(sparkConf)
-
-    // For Windows Users
-    System.setProperty("hadoop.home.dir", "D:\\winutils")
 
 
     // Turn off Info Logger for Console
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-    val input = sc.textFile("data/sentenceSample").map(line => {
+    val input = sc.textFile("data/abstracts.txt").map(line => {
       //Getting OpenIE Form of the word using lda.CoreNLP
 
       val t=CoreNLP.returnTriplets(line)
       t
     })
 
-    println(CoreNLP.returnTriplets("The dog has a lifespan of upto 10-12 years."))
-   // println(input.collect().mkString("\n"))
+    //println(CoreNLP.returnTriplets("The dog has a lifespan of upto 10-12 years."))
+
+    val writer = new PrintWriter(new File("data/triplets.txt"))
+    println(input.collect().mkString("\n"))
+    writer.write(input.collect().mkString("\n"))
+    writer.close()
 
 
 
